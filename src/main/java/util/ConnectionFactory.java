d96e4c07ad1043702d7292266b39d6fb5c2f3e9f
Max Dyachuk <51743080+maxdyachuk@users.noreply.github.com>
2020-03-05 15:42:23 +0200

    Configure db connection for heroku

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -6,17 +6,18 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private final static String DB_URL;
-    private final static String DB_USER;
-    private final static String DB_PASSWORD;
+    private final static String JDBC_DATABASE_URL;
+    private final static String JDBC_DATABASE_USERNAME;
+    private final static String JDBC_DATABASE_PASSWORD;
 
     static {
-        DB_URL = System.getenv("DB_URL");
-        DB_USER = System.getenv("DB_USER");
-        DB_PASSWORD = System.getenv("DB_PASSWORD");
+        JDBC_DATABASE_URL = System.getenv("JDBC_DATABASE_URL");
+        JDBC_DATABASE_USERNAME = System.getenv("JDBC_DATABASE_USERNAME");
+        JDBC_DATABASE_PASSWORD = System.getenv("JDBC_DATABASE_PASSWORD");
     }
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        return DriverManager.getConnection(JDBC_DATABASE_URL, JDBC_DATABASE_USERNAME,
+                JDBC_DATABASE_PASSWORD);
     }
 }
