43632dd79a4e440ce901326096fb2bf27e804e94
Max Dyachuk <51743080+maxdyachuk@users.noreply.github.com>
2020-03-10 16:43:31 +0200

    Fix typo

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -57,8 +57,8 @@ public class Bot extends AbilityBot {
                 "1. Add my email address `sharetokindlebot@gmail.com` to your [approved email list]" +
                 "(https://www.amazon.com/gp/help/customer/display.html?nodeId=201974240)\n" +
                 "2. Set your Kindle email address using /email command\n\n" +
-                "Supported formats: _doc_, _docx_, _pdf_, _txt_, _jpg_, _jpeg_, _png_, _bmp_," +
-                " _azw_, _mobi_, _rtf_, _prc_, _psz_";
+                "Supported formats: _doc_, _docx_, _pdf_, jpg_, _jpeg_, _png_, _bmp_," +
+                " _azw_, _mobi_, _rtf_";
         return Ability
                 .builder()
                 .name("start")
